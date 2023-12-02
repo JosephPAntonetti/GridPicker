1309e5b82e722debaf3f8d7702491174f8882faa
Joseph Antonetti <josephantonetti@Josephs-MacBook-Pro.local>
2023-12-02 15:27:01 -0500

    Initial Commit

---
+++ Sources/GridPicker/GridPicker.swift
@@ -0,0 +1,40 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Joseph Antonetti on 12/2/23.
+//
+
+import SwiftUI
+
+public struct GridPicker<Item, ItemView>: View where Item: Identifiable, Item : Equatable, ItemView : View {
+    
+    public let columns : Int
+    public let items : [Item]
+    
+    @Binding public var selected : Item
+
+    @ViewBuilder public let itemViewBuilder : (Item) -> ItemView
+    
+    private var gridItems : [GridItem] {
+        (0...columns).map({_ in GridItem(.flexible())})
+    }
+    
+    public var body: some View {
+        LazyVGrid(columns: gridItems) {
+            ForEach(items) {
+                item in
+                itemViewBuilder(item)
+                    .padding(.all, 4.0)
+                    .background(
+                        item == selected ? Circle().stroke(lineWidth: 2.0) : nil
+                    )
+                    .onTapGesture {
+                        withAnimation {
+                            selected = item
+                        }
+                    }
+            }
+        }
+    }
+}
